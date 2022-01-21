8087c199c3d9bbee54005c7c21af883370571a85
Kubilay <meneskaytemiz@hotmail.com>
2022-01-21 12:02:59 +0000

    one line hitomezashi

--- hitomezashi-golf.py
+++ hitomezashi-golf.py
@@ -2,9 +2,8 @@ from numpy.random import randint as t
 import matplotlib.pyplot as p
 e = enumerate
 
-s = lambda n,m: [([0,1]*((n // 2) + 1))[0+i:n+i] for i in [t(0,2) for _ in range(m+1)]]
 def hitomezashi(z):
-    [[[p.plot(*[[i, i + 1],[b,b]][::d]) for i,k in e(l) if k == 1] for b,l in e(s(*z[::d]))] for d in [-1,1]]
+    [[[p.plot(*[[i, i + 1],[b,b]][::d]) for i,k in e(l) if k == 1] for b,l in e([([0,1]*((z[::d][0]//2)+1))[0+i:z[::d][0]+i] for i in [t(0,2) for _ in range(z[::d][1]+1)]])] for d in [-1,1]]
     p.show()
 
 hitomezashi([20,20])
