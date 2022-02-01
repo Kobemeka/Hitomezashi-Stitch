834e57ee8e146da419a436366c7d5bc1b6733446
Kubilay <meneskaytemiz@hotmail.com>
2022-02-01 20:13:57 +0000

    new function names and text functions

--- hitomezashi.py
+++ hitomezashi.py
@@ -26,31 +26,48 @@ def create_lines(val,n):
     else:
         return ([1,0]*repeat)[0:n]
 
-def stack_lines(n,m):
+def random_stack_lines(n,m):
     '''
     creates lines from random numbers for given length and size
     '''
 
     return [create_lines(i,n) for i in random_numbers(m+1)]
 
-def draw_hline(lines,y,color,ax):
+def draw_hline(lines,y,color,ax,write,char):
     # draw a horizontal line
     [ax.plot([i, i + 1],[y,y],color=color) for i,v in enumerate(lines) if v == 1]
 
-def draw_hlines(stack_lines,color,ax):
-    # draw horizontal lines
-    [draw_hline(line,y,color,ax) for y,line in enumerate(stack_lines)]
+    if write == "default":
+        ax.text(-1,y,f"{lines[0]}",ha='center', va='center')
+    elif write == "chars":
+        ax.text(-1,y,f"{char}",ha='center', va='center')
 
-def draw_vline(lines,x,color,ax):
+def draw_hlines(random_stack_lines,color,ax,write="default",chars=""):
+    # draw horizontal lines
+    [draw_hline(line,y,color,ax,write,chars[y]) for y,line in enumerate(random_stack_lines)]
+
+def draw_vline(lines,x,color,ax,write,char):
     # draw a vertical line
     [ax.plot([x,x],[i, i + 1],color=color) for i,v in enumerate(lines) if v == 1]
-        
-def draw_vlines(stack_lines,color,ax):
-    # draw vertical lines
-    [draw_vline(line,x,color,ax) for x,line in enumerate(stack_lines)]
-        
 
-def hitomezashi(x,y,colorx="k",colory="k",save=True,file_name="hitomezashi-stitch.png",dpi=120):
+    if write == "default":
+        ax.text(x,-1,f"{lines[0]}",ha='center', va='center')
+    elif write == "chars":
+        ax.text(x,-1,f"{char}",ha='center', va='center')
+
+
+def draw_vlines(random_stack_lines,color,ax,write="default",chars=""):
+    # draw vertical lines
+    [draw_vline(line,x,color,ax,write,chars[x]) for x,line in enumerate(random_stack_lines)]
+
+def convert_vowel(char,ret = 1):
+    # takes a character and return ret if it is a vowel
+    if char.lower() in ["a","e","i","u"]:
+        return ret
+    else:
+        return not ret
+
+def random_hitomezashi(x,y,colorx="k",colory="k",save=True,file_name="hitomezashi-stitch.png",dpi=120):
     # creates hitomezashi stitch image for given x and y size
     # color is black by default for both vertical and horizontal lines
     # it saves the generated image to hitomezashi-stitch.png in current folder by default
@@ -58,9 +75,31 @@ def hitomezashi(x,y,colorx="k",colory="k",save=True,file_name="hitomezashi-stitc
     fig,ax = plt.subplots()
     ax.set_aspect(y/x)
  
-    draw_hlines(stack_lines(x,y),colorx,ax)
-    draw_vlines(stack_lines(y,x),colory,ax)
+    draw_hlines(random_stack_lines(x,y),colorx,ax)
+    draw_vlines(random_stack_lines(y,x),colory,ax)
     plt.axis("off")
     if save:
         plt.savefig(file_name,dpi=dpi)
     plt.show()
+
+def sentence_hitomezashi(sentence1,sentence2,rule,colorx="k",colory="k",save=True,file_name="hitomezashi-stitch.png",dpi=120):
+    fig,ax = plt.subplots()
+    x,y = len(sentence1), len(sentence2)
+    ax.set_aspect(y/x)
+    
+    if rule == "vowel":
+        hstacklines = [create_lines(convert_vowel(char),y-1) for char in sentence1]
+        vstacklines = [create_lines(convert_vowel(char),x-1) for char in sentence2]
+
+    draw_hlines(hstacklines,colorx,ax,write="chars",chars=sentence1)
+    draw_vlines(vstacklines,colory,ax,write="chars",chars=sentence2)
+
+    plt.axis("off")
+    if save:
+        plt.savefig(file_name,dpi=dpi)
+    plt.show()
+
+# random_hitomezashi(10,10)
+sentence_hitomezashi("wisharedoma hos geldiniz","kubilay aytemiz","vowel")
+
+# print(create_lines(convert_vowel("e"),5))
